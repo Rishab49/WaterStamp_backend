f9680932926a23ab3321ef8762a534b6783b8fdd
Rishab Raj <rishab.rishu49@gmail.com>
2023-02-27 19:56:48 +0530

    updated server.js #5

--- server.js
+++ server.js
@@ -19,11 +19,7 @@ registerFont("./assets/fonts/GeneralSans-Variable.ttf", {
 registerFont("./assets/fonts/Satoshi-Variable.ttf", { family: "Satoshi Variable" });
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 
 app.use(json({ limit: "50mb" }));
