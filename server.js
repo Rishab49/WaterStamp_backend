d5fa5aece5256bc98344fe31b407b5c1db4dfd3b
Rishab Raj <rishab.rishu49@gmail.com>
2023-02-27 18:53:52 +0530

    updated server.js #2

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 // app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://water-stamp.vercel.app/edit"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://water-stamp.vercel.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
